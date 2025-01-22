f110879d83627e39a27aaf7f86c9b5cd531d220d
KhangVinh <vinhkhang378@gmail.com>
2025-01-22 11:58:41 +0700

    config session

--- src/db/session.py
+++ src/db/session.py
@@ -1,3 +1,4 @@
+import aioodbc
 from typing import Callable, Optional
 
 from sqlalchemy import create_engine
@@ -9,39 +10,26 @@ from src.modules.base.entities import Base
 from src.common.consts import CommonConsts
 
 
-__async_engine: Optional[AsyncEngine] = None
+
+SYNC_DNS = "mssql+pyodbc://khang:123456@Khang/Broker?driver=ODBC+Driver+17+for+SQL+Server"
+ASYNC_DNS = CommonConsts.BACKEND_DNS
 
 def global_init():
-    global __async_engine
-    if __async_engine:
-        return
-
-    DNS = f"mssql+pyodbc://{CommonConsts.BACKEND_DNS}"
-    print(DNS)
-    ASYNC_DNS = f"mssql+aioodbc://{CommonConsts.BACKEND_DNS}"
-    sync_engine = create_engine(DNS, echo=False)
-    __async_engine = create_async_engine(ASYNC_DNS, echo=False)
-
-    # from src.modules.entities import User, FakeData
+    global SYNC_DNS
+    sync_engine = create_engine(SYNC_DNS, echo=False)
     Base.metadata.create_all(sync_engine)
-
-
 
 @asynccontextmanager
 async def async_session_scope():
+    global ASYNC_DNS
+
     """Provide a transactional scope around a series of operations."""
-    global __async_engine
-
-    if not __async_engine:
-        raise Exception("You have to call global_init() before using the session")
-
-    async_session: AsyncSession = AsyncSession(__async_engine)
-    async_session.sync_session.expire_on_commit = False
-
-    async with async_session as session:
-        try:
-            yield session
-            await session.commit()
-        except Exception:
-            await session.rollback()
-            raise
+    conn = await aioodbc.connect(dsn=ASYNC_DNS)
+    try:
+        yield conn
+        await conn.commit()
+    except Exception as e:
+        await conn.rollback()
+        raise
+    finally:
+        await conn.close()
--- test.py
+++ test.py
@@ -1,20 +1,28 @@
 import asyncio
 import aioodbc
 
+from src.db import session
 from src.common.consts import CommonConsts
 
 async def test_example():
-    dsn = f"{CommonConsts.BACKEND_DNS}"
-    conn = await aioodbc.connect(dsn=dsn)
-
-    cur = await conn.cursor()
-    await cur.execute("SELECT 42 AS age;")
-    rows = await cur.fetchall()
-    print(rows)
-    print(rows[0])
-    print(rows[0].age)
-    await cur.close()
-    await conn.close()
+    session.global_init()
+    async with session.async_session_scope() as conn:
+        async with conn.cursor() as cur:
+            # await cursor.execute("SELECT * FROM dbo.users")
+            await cur.execute("SELECT 42 AS age;")
+            rows = await cur.fetchall()
+            for row in rows:
+                print(row)
 
 
 asyncio.run(test_example())
+
+# import pyodbc
+
+# conn_str = "DRIVER={ODBC Driver 17 for SQL Server};Server=Khang,1433;Database=Broker;Trusted_Connection=yes;"
+# try:
+#     conn = pyodbc.connect(conn_str)
+#     print("Connection successful")
+#     conn.close()
+# except pyodbc.Error as ex:
+#     print("Connection failed:", ex)
